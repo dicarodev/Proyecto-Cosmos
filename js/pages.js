594d4b36c35caccf1072a33b5407de6345a0a7ce
MrWiskers06 <diego.carramolino06@gmail.com>
2023-04-27 22:28:08 +0200

    Modifico estilos

--- js/pages.js
+++ js/pages.js
@@ -1,3 +1,4 @@
+//Animacion ScrollTop
 $(window).scroll(function() {
     //Compara la posición vertical del usuario con la altura total de la página
     if ($(this).scrollTop() >= document.body.scrollHeight - $(window).height()) {
@@ -12,20 +13,12 @@ $(window).scroll(function() {
 $(document).ready(function() {
     $(".cohete").on("click", function() {
 
-        //Desplaza la página hacia la parte superior de la página con una animación suave durante 2 segundos cuando se hace clic en el cohete.
-        $("html, body").animate({ scrollTop: 0 }, 2000, function() { 
+        //Desplaza la página hacia la parte superior de la página con una animación suave durante 3 segundos cuando se hace clic en el cohete.
+        $("html, body").animate({ scrollTop: 0 }, 3000, function() { 
 
             //Vuelve al inicio de la página cuando se completa la animación de desplazamiento.
             window.location.href = "#top"; 
         });
-
-        //Agrega la clase `miOrbita` al cohete para agregar una animación de rotación.
-        $(".cohete").addClass("miOrbita"); 
-
-        //Elimina la clase `miOrbita` del cohete después de 2 segundos para detener la animación de rotación.
-        setTimeout(function() {
-            $(".cohete").removeClass("miOrbita"); 
-        }, 2000);
 
         //Previene el comportamiento predeterminado del enlace al hacer clic en el cohete.
         return false; 
