64a4dd9b530b65edd1121357105fad54e79bce9a
scnredu <scnredu@gmail.com>
2023-05-28 07:06:39 +0200

    inserción del archivo menu.js y retoques en el footer

---
+++ js/menu.js
@@ -0,0 +1,5 @@
+const btnMenu = document.querySelector("#btnMenu");
+const menu = document.querySelector("#menu");
+btnMenu.addEventListener("click", function() {
+          menu.classList.toggle("mostrar");
+});
